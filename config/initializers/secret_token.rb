aeeca5ad49fbb75c8a008c2220357ede61c30ab6
Donovan <donovan@donovanlind.com>
2014-01-25 11:40:04 -0600

    Init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marbles::Application.config.secret_key_base = '918468e66ac3be44d3c58d3366ea2b188a28024c0f3d61cd48b1fc2b13edf2ac7184956ecb0ceabd162e06e0d7a0154946f2469b8fa65f84090fbb7a2428da6a'
