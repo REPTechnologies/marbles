6486e1478a07d2f99d0d37ca99eaa89790100a0d
Donovan <donovan@donovan.mn>
2014-03-16 18:18:40 -0500

    Associate organization with a single package

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,3 @@
 class Package < ActiveRecord::Base
+  has_many :organizations, :inverse_of => :plan
 end
--- db/migrate/20140316215610_create_organizations.rb
+++ db/migrate/20140316215610_create_organizations.rb
@@ -3,6 +3,7 @@ class CreateOrganizations < ActiveRecord::Migration
     create_table :organizations do |t|
       t.string :name
       t.belongs_to :owner, index: true
+      t.belongs_to :plan, index: true
 
       t.timestamps
     end
