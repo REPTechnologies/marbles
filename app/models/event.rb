2138ab3dfe2ebffe8336cc20b1cd352af00ac0d4
Donovan <donovan@donovan.mn>
2014-03-16 17:59:09 -0500

    Associate events and attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,5 @@ class Event < ActiveRecord::Base
   belongs_to :secondary_focus, :class_name => "Focus", :foreign_key => "secondary_focus_id"
   enum type: [ :presentation, :networking, :socializing, :discussion, :other ]
   has_and_belongs_to_many :scopes
+  has_and_belongs_to_many :attendees, :class_name => "User"
 end
