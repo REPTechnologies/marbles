dcc2591475d0697e6ed131b0f1bdcbad77e33944
Donovan Mikrot <donovan@donovan.mn>
2014-03-29 18:59:59 -0500

    I was wrong about uncommenting these

--- db/migrate/20140329211042_create_join_table_question_secondary_focus.rb
+++ db/migrate/20140329211042_create_join_table_question_secondary_focus.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableQuestionSecondaryFocus < ActiveRecord::Migration
   def change
     create_join_table :questions, :secondary_focus do |t|
-       t.index [:question_id, :secondary_focus_id]
-       t.index [:secondary_focus_id, :question_id]
+      # t.index [:question_id, :secondary_focus_id]
+      # t.index [:secondary_focus_id, :question_id]
     end
   end
 end
