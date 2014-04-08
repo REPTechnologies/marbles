ade2af5b2dc2209248ccc5c1bac1c519e9a6ed40
Donovan Mikrot <donovan@donovan.mn>
2014-04-08 11:43:32 -0500

    Update usage of gon since view parameter is deprecated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     gon.rabl
     @user = current_user
-    gon.rabl "app/views/users/show.json.rabl", as: "current_user"
+    gon.rabl template: "app/views/users/show.json.rabl", as: "current_user"
   end
 end
