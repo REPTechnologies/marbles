e955805ad86dffaaf5ad31c9232a4d1e0ccfdd71
Donovan Mikrot <donovan@donovan.mn>
2014-04-08 18:29:43 -0500

    Update rails config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Marbles::Application.config.session_store :cookie_store, key: '_marbles_session'
+Rails.application.config.session_store :cookie_store, key: '_marbles_session'
