1bd47448e2f121a24f3d9a8188726e794a3523a5
Donovan Mikrot <donovan@donovan.mn>
2014-06-28 14:10:52 -0500

    Add link to switch between adding new org or using existing

--- app/assets/javascripts/apps/add/organization/organization_view.js
+++ app/assets/javascripts/apps/add/organization/organization_view.js
@@ -5,8 +5,22 @@
 
   Marbles.module('AddApp.New.Organization', function (Organization, Marbles, Backbone, Marionette, $, _) {
 
+    function toggleOrganizationInput() {
+      this.ui.orgInput.toggleClass('hidden');
+      this.ui.orgInput.filter('select.hidden, input.hidden').prop('disabled', true);
+      this.ui.orgInput.filter('select, input').not('.hidden').prop('disabled', false);
+    }
+
     Organization.Picker = Marionette.ItemView.extend({
-      template: 'add/organization/picker'
+      template: 'add/organization/picker',
+      ui: {
+        toggleOrg: '.toggleOrganizationLink',
+        orgInput: '.organizationInput'
+      },
+      events: {
+        'click @ui.toggleOrg': 'toggleOrg'
+      },
+      toggleOrg: toggleOrganizationInput
     });
   });
 
