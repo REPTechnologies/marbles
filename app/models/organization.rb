241560a4362a96069b3d71c1e8e3d30f97a596ec
Donovan Mikrot <donovan@donovan.mn>
2014-08-16 22:17:06 -0500

    Add more cards to organization page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,9 @@ class Organization < ActiveRecord::Base
   has_many :events, :inverse_of => :organization
   has_and_belongs_to_many :users
   belongs_to :plan, :class_name => "Package", :inverse_of => :organizations
+
+  def num_followers
+    256
+  end
+
 end
--- app/views/v1/organizations/show.json.rabl
+++ app/views/v1/organizations/show.json.rabl
@@ -1,3 +1,3 @@
 object @organization
 
-attributes :id, :name, :event_ids, :owner_id, :user_ids
+attributes :id, :name, :event_ids, :owner_id, :user_ids, :num_followers
