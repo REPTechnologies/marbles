2e5153fb6a3fe654665174bbe2b305cd40948bef
Donovan Mikrot <donovan@donovan.mn>
2014-09-01 15:49:48 -0500

    Trends page basic markup

--- app/assets/javascripts/apps/trends/show/show_layout.js
+++ app/assets/javascripts/apps/trends/show/show_layout.js
@@ -1,11 +1,9 @@
-/*jslint indent: 2, nomen: true*/
-/*global Marbles */
 (function () {
-  "use strict";
+  'use strict';
 
-  Marbles.module("TrendsApp.Show", function (Show, Marbles, Backbone, Marionette, $, _) {
+  Marbles.module('TrendsApp.Show', function (Show, Marbles, Backbone, Marionette, $, _) {
     Show.Layout = Marionette.LayoutView.extend({
-      template: "trends/show/show"
+      template: 'trends/show/show'
     });
   });
 
