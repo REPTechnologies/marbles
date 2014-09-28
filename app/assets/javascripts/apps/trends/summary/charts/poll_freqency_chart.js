a7e9bbc56a4b5ade01270206dfbe07dbde39e36e
Donovan Mikrot <donovan@donovan.mn>
2014-09-28 12:24:45 -0500

    Refactor Poll Frequency Chart into separate class

---
+++ app/assets/javascripts/apps/trends/summary/charts/poll_freqency_chart.js
@@ -0,0 +1,69 @@
+(function () {
+  'use strict';
+
+  Marbles.module('TrendsApp.Summary.Chart', function (Chart, Marbles, Backbone, Marionette, $, _) {
+    function PollFrequencyChart(config) {
+      this.ndxPoll = config.ndxPoll;
+      this.pollMonthDim = this.ndxPoll.dimension(_.property('month'));
+      this.pollCountByMonth = this.pollMonthDim.group().reduce.apply(null, this.getReducers());
+
+      this.initialize(config);
+    }
+
+    PollFrequencyChart.prototype.initialize = function (config) {
+      this.chart = dc.barChart(config.container).options(this.getChartOptions(config));
+      this.chart.xAxis().tickFormat(d3.time.format('%b'));
+    };
+
+    PollFrequencyChart.prototype.getReducers = function () {
+      return [
+        function addFn(p, v) {
+          var pollId = v.poll.id;
+          if (!_.contains(p.ids, pollId)) {
+            p.ids.push(pollId);
+            ++p.count;
+          }
+          return p;
+        }, function removeFn(p, v) {
+          var pollId = v.poll.id;
+          if (_.contains(p.ids, pollId)) {
+            p.ids = _.without(p.ids, pollId);
+            --p.count;
+          }
+          return p;
+        }, function initFn() {
+          return {ids: [], count: 0};
+        }
+      ];
+    };
+
+    PollFrequencyChart.prototype.getXScale = function (config) {
+      return d3.time.scale().domain([
+        new Date(2014, 0, 1),
+        new Date(2014, 11, 31)
+      ]);
+    };
+
+    PollFrequencyChart.prototype.getChartOptions = function (config) {
+      return _.extend({
+        width: 360,
+        height: 200,
+        dimension: this.pollMonthDim,
+        group: this.pollCountByMonth,
+        valueAccessor: _.compose(_.property('count'), _.property('value')),
+        x: this.getXScale(config),
+        xUnits: d3.time.months,
+        brushOn: false,
+        elasticY: true,
+      }, config.chartOptions);
+    };
+
+    PollFrequencyChart.prototype.destroy = function () {
+      this.pollCountByMonth.dispose();
+      this.pollMonthDim.dispose();
+    };
+
+    Chart.PollFrequencyChart = PollFrequencyChart;
+  });
+
+}());
--- app/assets/javascripts/apps/trends/summary/summary_view.js
+++ app/assets/javascripts/apps/trends/summary/summary_view.js
@@ -7,51 +7,15 @@
       initialize: function initializeFn(options) {
         this.ndxPoll = options.ndxPoll;
         this.ndxEvent = options.ndxEvent;
-        this.pollMonthDim = this.ndxPoll.dimension(_.property('month'));
-        this.pollCountByMonth = this.pollMonthDim.group().reduce.apply(null, this.pollCountReducers);
       },
       onShow: function onShowFn() {
-        this.pollFreqChart = this.initPollFreqChart();
+        this.pollFreqChart = new Summary.Chart.PollFrequencyChart({
+          container: '#poll-frequency-chart',
+          ndxPoll: this.ndxPoll
+        });
       },
       onDestroy: function onDestroyFn() {
-        this.pollMonthDim.dispose();
-      },
-      pollCountReducers: [
-        function addFn(p, v) {
-          var pollId = v.poll.id;
-          if (!_.contains(p.ids, pollId)) {
-            p.ids.push(pollId);
-            ++p.count;
-          }
-          return p;
-        }, function removeFn(p, v) {
-          var pollId = v.poll.id;
-          if (_.contains(p.ids, pollId)) {
-            p.ids = _.without(p.ids, pollId);
-            --p.count;
-          }
-          return p;
-        }, function initFn() {
-          return {ids: [], count: 0};
-        }
-      ],
-      initPollFreqChart: function initPollFreqChartFn() {
-        var xScale = d3.time.scale().domain([
-          new Date(2014, 0, 1),
-          new Date(2014, 11, 31)
-        ]);
-
-        return dc.barChart('#poll-frequecy-chart').options({
-          width: 360,
-          height: 200,
-          dimension: this.pollMonthDim,
-          group: this.pollCountByMonth,
-          valueAccessor: _.compose(_.property('count'), _.property('value')),
-          x: xScale,
-          xUnits: d3.time.months,
-          brushOn: false,
-          elasticY: true,
-        }).xAxis().tickFormat(d3.time.format('%b'));
+        this.pollFreqChart.destroy();
       }
     });
   });
