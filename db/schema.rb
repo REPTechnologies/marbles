955e3c81d4e8967aee3ca762d4a69da20e6ba5bd
Donovan Mikrot <donovan@donovan.mn>
2015-01-27 21:56:16 -0600

    Require whitelisted email in AlphaInvites table for registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150125170121) do
+ActiveRecord::Schema.define(version: 20150128033911) do
+
+  create_table "alpha_invites", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "answers", force: true do |t|
     t.integer  "slider"
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,4 @@
 # This will be run during `rake db:seed` in the :development environment.
 def seeds_env
-  [User, Organization, Event]
+  [User, Organization, Event, AlphaInvite]
 end
